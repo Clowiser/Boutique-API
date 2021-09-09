be87afeee5906ac13daa243ce37f3c1980f9695e
Anthony-B404 <anthony.bette@le-campus-numerique.fr>
2021-09-09 14:27:44 +0200

    bug correction

--- API/produit.php
+++ API/produit.php
@@ -4,3 +4,9 @@ require('../html/fonctions.php');
 if(isset($_GET['cat'])){
     getCat($_GET['cat']);
 }
+elseif (isset($_GET['price'])) {
+    getProductsPS();
+}
+else{
+    getProducts();
+}
--- html/fonctions.php
+++ html/fonctions.php
@@ -5,43 +5,43 @@ function afficheArticle($article){ // pour afficher les articles spécifiquement
     $reponse = $bdd->query("SELECT * FROM articles WHERE idArticles='$article'"); // réquète sélectionne tout dans la table articles ou l'idArticles=$article
 
     while ($donnees = $reponse->fetch()){
-?>
+        ?>
         <div class='contain'> <p> <?php echo $donnees['nomArticle']?></p>
-        <?php echo "<img src=".$donnees['imgArticle']." width=200px>"; ?>
-        <p> <?php echo $donnees['descArticle']?></p>
-        <p> <?php echo $donnees['prixArticle']?> € </p> </div> 
-<?php
+            <?php echo "<img src=".$donnees['imgArticle']." width=200px>"; ?>
+            <p> <?php echo $donnees['descArticle']?></p>
+            <p> <?php echo $donnees['prixArticle']?> € </p> </div>
+        <?php
     }
 }
 
 // Création du panier via la BDD
-function affichePanier($panier){ 
+function affichePanier($panier){
     $bdd = new PDO('mysql:host=localhost;dbname=bddex;charset=utf8', 'JessiRig', 'evolPHP2+');
     $reponse = $bdd->query("SELECT * FROM articles WHERE idArticles='$panier'"); // réquète sélectionne tout dans la table articles ou l'idArticles=$panier
 
     while ($donnees = $reponse->fetch()){
-?>
+        ?>
         <div class='contain'> <p> <?php echo $donnees['nomArticle']?></p>
-        <?php echo "<img src=".$donnees['imgArticle']." width=200px>"; ?>
-        <p> <?php echo $donnees['descArticle']?></p>
-        <p> <?php echo $donnees['prixArticle']?> €  </p> </div>
-<?php
+            <?php echo "<img src=".$donnees['imgArticle']." width=200px>"; ?>
+            <p> <?php echo $donnees['descArticle']?></p>
+            <p> <?php echo $donnees['prixArticle']?> €  </p> </div>
+        <?php
     }
 }
 
 // Total du panier quand on modifie la quantité | revoir la seconde partie pour la modification de la quantité
-function totalPanier($id,$quantite) { 
+function totalPanier($id,$quantite) {
     $bdd = new PDO('mysql:host=localhost;dbname=bddex;charset=utf8', 'JessiRig', 'evolPHP2+');
     $reponse = $bdd->query("SELECT * FROM articles WHERE idArticles='$id'"); // réquète sélectionne tout dans la table articles ou l'idArticles=$article
 
     while ($donnees = $reponse->fetch()){
-?>
-       <div class="contain"> <?php $total=$quantite*$donnees['prixArticle']; 
+        ?>
+        <div class="contain"> <?php $total=$quantite*$donnees['prixArticle'];
         echo'<form method="POST" action="index.php?page=panier.php">',
-        '<input type="number" name="quantite" min="0" value="'.$quantite.'" >',
-        '<input type="hidden" name="article" value="'.$id.'">',
-        '<input type="hidden" name="prix" value="'.$donnees['prixArticle'].'">',
-        '<input type="submit" name="modifQuantite" value="Modifier la quantité"> </form>','<p> Sous-total : '.$total.' € </p>','</div>'; 
+            '<input type="number" name="quantite" min="0" value="'.$quantite.'" >',
+            '<input type="hidden" name="article" value="'.$id.'">',
+            '<input type="hidden" name="prix" value="'.$donnees['prixArticle'].'">',
+        '<input type="submit" name="modifQuantite" value="Modifier la quantité"> </form>','<p> Sous-total : '.$total.' € </p>','</div>';
     }
 }?>
 
@@ -52,16 +52,16 @@ function totaltotal(){
     $montanttotal = 0; // cette var nous sert à calculer le montant total des articles
     while ($donnees = $reponse->fetch()){
         foreach ($_SESSION['panier'] as $k => $v) { // comment l'ancienne fonction, on parcout la table article via foreach (avec Sessions qui stocke les données indiv) avec var k associé à var v soit $key => $value ($k parcours et stock dans $v) soit $k => $v = id => 1 etc
-                if($donnees['idArticles'] == $_SESSION['panier'][$k]['id']){ // si l'id de l'element du tableau est égal au donnees du tableau panier
-            $total=$donnees['prixArticle']*$_SESSION['panier'][$k]['quantite']; // ce n'est plus l'element du tableau ($element['id'] ou $element['prix']) que l'on parcourt mais les données de la table via la connexion à la BDD 
-            $montanttotal=$total+$montanttotal; // nouveau montanttotal = total(donc )
+            if($donnees['idArticles'] == $_SESSION['panier'][$k]['id']){ // si l'id de l'element du tableau est égal au donnees du tableau panier
+                $total=$donnees['prixArticle']*$_SESSION['panier'][$k]['quantite']; // ce n'est plus l'element du tableau ($element['id'] ou $element['prix']) que l'on parcourt mais les données de la table via la connexion à la BDD
+                $montanttotal=$total+$montanttotal; // nouveau montanttotal = total(donc )
+            }
+        }
     }
-}   
-}
-echo "<div class='contain'><p>Le montant total de la commande est de : ".$montanttotal."€</p></div>";
+    echo "<div class='contain'><p>Le montant total de la commande est de : ".$montanttotal."€</p></div>";
 }
 
-function modifQuantite($id,$quantite,$ajout){ // fonction pour modifier la quantité de l'article avec les paramètres id et quantité que l'on va ajouter dans le panier 
+function modifQuantite($id,$quantite,$ajout){ // fonction pour modifier la quantité de l'article avec les paramètres id et quantité que l'on va ajouter dans le panier
     $exist = false;
     if (!isset($_SESSION['panier'])){
         $_SESSION['panier'][] = ['id' => $_POST['article'], 'quantite' => $_POST['quantite']];
@@ -79,19 +79,29 @@ function modifQuantite($id,$quantite,$ajout){ // fonction pour modifier la quant
         }
     }
 }
-    
+
 
 function viderPanier($panier) { // variable pour vider le panier
     unset($_SESSION['panier']); // unset : supprime le panier
 }
 
 
-////
-
+function getProducts()
+{
+    $bdd = new PDO('mysql:host=localhost;dbname=bddex;charset=utf8', 'JessiRig', 'evolPHP2+', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    $reponse = $bdd->query("SELECT * FROM articles");
+    $donnees = $reponse->fetchAll(PDO::FETCH_ASSOC);
+    header('Content-Type: application/json');
+    echo json_encode($donnees, JSON_PRETTY_PRINT);
+}
 function getProductsPS() ////PS = prix stock
 {
     $bdd = new PDO('mysql:host=localhost;dbname=bddex;charset=utf8', 'JessiRig', 'evolPHP2+', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
     $reponse = $bdd->query("SELECT prixArticle, quantiteDispo FROM articles");
+
+    $donnees = $reponse->fetchAll(PDO::FETCH_ASSOC);
+    header('Content-Type: application/json');
+    echo json_encode($donnees, JSON_PRETTY_PRINT);
 }
 
 function getCat($cat)
